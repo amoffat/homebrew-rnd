67310b87f09b498abce6545ea1ac5dd8c2091c24
Andrew Moffat <arwmoffat@gmail.com>
2021-01-03 07:30:28 -0800

    v0.1.4

--- supertag.rb
+++ supertag.rb
@@ -1,8 +1,8 @@
 class Supertag < Formula
   desc "Tag-based filesystem"
   homepage "https://amoffat.github.io/supertag/"
-  url "https://github.com/amoffat/supertag/archive/v0.1.3.tar.gz"
-  sha256 "3ecec8d40dbebfa8f865d68af09a4e92ae01d029a69aa59f99c0e1223b0d249a"
+  url "https://github.com/amoffat/supertag/archive/v0.1.4.tar.gz"
+  sha256 "d8a724e0a85c0f2830eb943a17ed8f554f16a2f767392d64ded7251aa76b5715"
   license "AGPL-3.0-or-later"
 
   depends_on osxfuse: "3.11.2"
